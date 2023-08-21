9e35340e950a1770d33c3fcd32290f2940c6e49f
mahesh <maheshbabugolla9@gmail.com>
2023-08-21 19:45:54 -0400

    install-soft.sh

--- install-soft.sh
+++ install-soft.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 Date=$(date +%F)
 echo $Date
+Logdirectory=/home/centos/shell-scripts/logs
+Script_name=$0
+Logfile=$Logdirectory+$Script_name-$Date.log
+Userid=$(id -u)
+R="\e[31m"
+G="\e[31m"
+N="\e[om"
+Y="\e[33m"
+
+if[ $Userid -ne 0]
+then
+    echo -e "$R Error: $N Please login with root user"
+    exit 1
+fi
