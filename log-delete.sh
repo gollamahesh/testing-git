275d5a12eb327df78d9e2d005ea173f6ac5e90e7
mahesh <maheshbabugolla9@gmail.com>
2023-08-23 19:24:25 -0400

    logs-delete

--- log-delete.sh
+++ log-delete.sh
@@ -2,4 +2,4 @@ Applog_directory_Name=/home/centos/app-logs
 Date=$(date +%F)
 Script_name=$0
 Files_to_delete=$(find . -name "*.log" -type f -mtime +20)
-echo $Files_to_delete
+echo "$Files_to_delete"
